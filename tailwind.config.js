39c31eccbcd0347102e8b3c4521e737dc1a65da9
Tom Meese <tom@meeses.com>
2025-09-09 07:43:18 -0700

    setup an action to build tailwind to it isn't pulled from the CDN

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/**/*.{html,js}"],   // scan your index.html + inline <script>
+  theme: { extend: {} },
+  plugins: [],
+};
