562984441ad68be44604420f8c3344bb0e686815
Tom Meese <tom@meeses.com>
2025-09-10 15:00:21 -0700

    added a router to expose the demo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-// src/App.tsx
 import React from 'react'
-import DemoGuardrails from './pages/DemoGuardrails' // or './pages/DemoGuardrails' if App.tsx is already under src
+import { Routes, Route, Link } from 'react-router-dom'
+import DemoGuardrails from './pages/DemoGuardrails'
+// If your existing root app is in another component, import it here:
+// import ExistingApp from './ExistingApp'
+
+function ExistingApp() {
+  // temporary stub if you don’t have a component handy:
+  return <div style={{padding:16}}>Existing app home. Go to <Link to="/demo">Demo</Link></div>
+}
 
 export default function App() {
-  return <DemoGuardrails />
+  return (
+    <Routes>
+      <Route path="/" element={<ExistingApp />} />
+      <Route path="/demo" element={<DemoGuardrails />} />
+      <Route path="*" element={<ExistingApp />} />
+    </Routes>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/intervals-icu-bulk-uploader-planned-workouts">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
