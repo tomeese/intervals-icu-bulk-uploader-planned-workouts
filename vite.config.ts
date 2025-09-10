cba7ed7a08517bfc81b91e5c275b3dd344a06f49
Tom Meese <tom@meeses.com>
2025-09-10 09:38:51 -0700

    added auto upload on main and wired up recent changes to gh pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// IMPORTANT: set base to your repo name when hosting on GitHub Pages
+// If your repo is "intervals-icu-bulk-uploader-planned-workouts", the base should be:
+// '/intervals-icu-bulk-uploader-planned-workouts/'
 export default defineConfig({
-  plugins: [react()]
+plugins: [react()],
+base: '/intervals-icu-bulk-uploader-planned-workouts/',
 })
