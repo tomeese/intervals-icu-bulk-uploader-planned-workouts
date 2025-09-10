325d0fd2014c00b91ff6b1bd10e19f8599fe3e1a
Tom Meese <tom@meeses.com>
2025-09-10 16:52:03 -0700

    added debug statement to determine if the page is loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+// TEMP: boot probe
+console.log("BOOT: main.tsx running");
+document.getElementById('root')!.textContent = "Loading…";
+
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
